787a095078cb277a5cf16de3a019e81b50ab87dc
souz-dev <souz.developer@gmail.com>
2024-07-08 17:43:26 -0300

    b.o no commit

--- src/view/layouts/auth-styles.ts
+++ src/view/layouts/auth-styles.ts
@@ -4,3 +4,23 @@ export const Container = styled.main`
   width: 100vw;
   height: 100vh;
 `;
+
+export const Section = styled.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+
+  @media (min-width: 1024px) {
+    width: 50%;
+  }
+`;
+
+export const Div = styled.div`
+  max-width: 504px;
+  width: 100%;
+  padding: 0 2rem;
+`;
